4bdd4512084b09ccd0e8f9d0caa81420d8fc0ce5
sharan-2003 <chittisharan11@gmail.com>
2024-06-28 21:20:21 +0530

    TODOAPP

---
+++ client/src/Views/TODO/TODO.js
@@ -0,0 +1,223 @@
+import { useEffect, useState } from 'react'
+import Styles from './TODO.module.css'
+import { dummy } from './dummy'
+import axios from 'axios';
+
+export function TODO(props) {
+
+    const [newTodo, setNewTodo] = useState('')
+    const [todoData, setTodoData] = useState(dummy);
+    const [loading, setLoading] = useState(true);
+    const [editTodo, setEditTodo] = useState(null);
+    const [editTitle, setEditTitle] = useState('');
+
+    useEffect(() => {
+        const fetchTodo = async () => {
+            const apiData = await getTodo()
+            setTodoData(apiData);
+            setLoading(false)
+        }
+        fetchTodo();
+    }, [])
+
+    const getTodo = async () => {
+        const options = {
+            method: "GET",
+            url: `http://localhost:8000/api/todo`,
+            headers: {
+                accept: "application/json",
+            }
+        }
+        try {
+            const response = await axios.request(options)
+            return response.data
+        } catch (err) {
+            console.log(err);
+            return []; // return an empty array in case of error
+        }
+    }
+
+    const addTodo = () => {
+        const options = {
+            method: "POST",
+            url: `http://localhost:8000/api/todo`,
+            headers: {
+                accept: "application/json",
+            },
+            data: {
+                title: newTodo
+            }
+        }
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data)
+                setTodoData(prevData => [...prevData, response.data.newTodo])
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const deleteTodo = (id) => {
+        const options = {
+            method: "DELETE",
+            url: `http://localhost:8000/api/todo/${id}`,
+            headers: {
+                accept: "application/json",
+            }
+        }
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data)
+                setTodoData(prevData => prevData.filter(todo => todo._id !== id))
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    const updateTodo = (id) => {
+        const todoToUpdate = todoData.find(todo => todo._id === id)
+        const options = {
+            method: "PATCH",
+            url: `http://localhost:8000/api/todo/${id}`,
+            headers: {
+                accept: "application/json",
+            },
+            data: {
+                ...todoToUpdate,
+                done: !todoToUpdate.done
+            }
+        }
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data)
+                setTodoData(prevData => prevData.map(todo => todo._id === id ? response.data : todo))
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    const editTodoItem = (id) => {
+        const todoToEdit = todoData.find(todo => todo._id === id)
+        setEditTodo(id)
+        setEditTitle(todoToEdit.title)
+    }
+
+    const saveEditTodo = (id) => {
+        const options = {
+            method: "PATCH",
+            url: `http://localhost:8000/api/todo/${id}`,
+            headers: {
+                accept: "application/json",
+            },
+            data: {
+                title: editTitle
+            }
+        }
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data)
+                setTodoData(prevData => prevData.map(todo => todo._id === id ? response.data : todo))
+                setEditTodo(null)
+                setEditTitle('')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    };
+
+    return (
+        <div className={Styles.ancestorContainer}>
+            <div className={Styles.headerContainer}>
+                <h1>
+                    Tasks
+                </h1>
+                <span>
+                    <input
+                        className={Styles.todoInput}
+                        type='text'
+                        name='New Todo'
+                        value={newTodo}
+                        onChange={(event) => {
+                            setNewTodo(event.target.value)
+                        }}
+                    />
+                    <button
+                        id='addButton'
+                        name='add'
+                        className={Styles.addButton}
+                        onClick={() => {
+                            addTodo()
+                            setNewTodo('')
+                        }}
+                    >
+                        + New Todo
+                    </button>
+                </span>
+            </div>
+            <div id='todoContainer' className={Styles.todoContainer}>
+                {loading ? (
+                    <p style={{ color: 'white' }}>Loading...</p>
+                ) : (
+                    todoData.length > 0 ? (
+                        todoData.map((entry, index) => (
+                            <div key={entry._id} className={Styles.todo}>
+                                {editTodo === entry._id ? (
+                                    <>
+                                        <input
+                                            type='text'
+                                            value={editTitle}
+                                            onChange={(event) => setEditTitle(event.target.value)}
+                                        />
+                                        <button onClick={() => saveEditTodo(entry._id)}>Save</button>
+                                        <button onClick={() => {
+                                            setEditTodo(null);
+                                            setEditTitle('');
+                                        }}>Cancel</button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className={Styles.infoContainer}>
+                                            <input
+                                                type='checkbox'
+                                                checked={entry.done}
+                                                onChange={() => {
+                                                    updateTodo(entry._id);
+                                                }}
+                                            />
+                                            {entry.title}
+                                        </span>
+                                        <span
+                                            style={{ cursor: 'pointer' }}
+                                            onClick={() => {
+                                                deleteTodo(entry._id);
+                                            }}
+                                        >
+                                            Delete
+                                        </span>
+                                        <span
+                                            style={{ cursor: 'pointer' }}
+                                            onClick={() => {
+                                                editTodoItem(entry._id);
+                                            }}
+                                        >
+                                            Edit
+                                        </span>
+                                    </>
+                                )}
+                            </div>
+                        ))
+                    ) : (
+                        <p className={Styles.noTodoMessage}>No tasks available. Please add a new task.</p>
+                    )
+                )}
+            </div>
+        </div>
+    );
+}
